085af2bf84cef856cd2a83e561882226844fa1ac
Wisdom Matthew <wisdommatthew08@gmail.com>
2024-07-24 00:40:45 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// variable
+// const accordion = document.getElementsByClassName("content-container");
+// looping through the answer
+// for (i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//   });
+// }
